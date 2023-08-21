b449c70b0fdaa5066a90f0ce227c169aa91be3bf
unknown <towhidulsislam@gmail.com>
2023-08-21 17:38:06 +0600

    Create user API

--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'Name is must be given'] },
+    fullName: { type: String, required: [true, 'Name is must be given'] },
     email: { type: String, required: [true, 'Email is must be given'] },
-    phone: { type: String, required: false},
-    phone: { type: String, required: [true, 'Password is must be given']},
+    phoneNumber: { type: String, required: false},
+    gender: { type: String, required: [true, 'Password is must be given']},
     address: { type: String, required: false},
+    dateOfBirth: { type: String, required: false},
+    password: { type: String, required: true},
+    KYC: { type: Object, required: false},
+    RFC: { type: String, required: false},
+    image: { type: Object, required: false},
+    role: { type: String, enum: ['user', 'admin', 'unknown', 'host'], default: 'unknown' },
   },{
     toJSON: {
       transform(doc, ret) {
