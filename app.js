2032e1ea5cb559e38bc48d88d0e2f870eba578e7
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-22 18:09:15 +0600

    Cars CRUD Api Added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/userRouter');
+const carRouter = require('./routes/carRouter');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -14,20 +15,21 @@ mongoose.connect(process.env.MONGODB_CONNECTION, {
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: [process.env.ALLOWED_CLIENT_URLS , process.env.ALLOWED_CLIENT_URLS_2],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }));
+  origin: [process.env.ALLOWED_CLIENT_URLS, process.env.ALLOWED_CLIENT_URLS_2],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/car', carRouter);
 
 
 // Start the server
-app.listen(3001, '103.161.9.43',() => {
-    console.log('Server started on port 3001');
+app.listen(3001, '103.161.9.106', () => {
+  console.log('Server started on port 3001');
 });
 
 module.exports = app;
---
+++ models/Car.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    carModelName: { type: String, required: [true, 'Name is must be Required'] },
+    image: { type: String, required: [true, 'Image is must be Required'] },
+    year: { type: Number, required: [true, 'Year is must be Required'] },
+    carLicenseNumber: { type: String, required: [true, 'Car License Number is must be Required'] },
+    carDescription: { type: String, required: [true, 'Car Description is must be Required'] },
+    insuranceStartDate: { type: String, required: [true, 'Start Date is must be Required'] },
+    insuranceEndDate: { type: String, required: [true, 'End Date is must be Required'] },
+    // carLicenseImage: { type: Object, required: [true, 'Car License Image is must be Required'] },
+    carLicenseImage: { type: String, required: [true, 'Car License Image is must be Required'] },
+    carColor: { type: String, required: false },
+    carDoors: { type: String, required: false },
+    carSeats: { type: String, required: false },
+    totalRun: { type: String, required: false },
+    gearType: { type: String, enum: ['Baby Car Seat', 'Sunroof', 'Bluetooth', 'GPS'], default: 'Baby Car Seat' },
+},
+    { timestamps: true },
+
+);
+
+module.exports = mongoose.model('Car', carSchema);
