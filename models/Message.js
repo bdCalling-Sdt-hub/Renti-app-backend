73b860f45fd16d22be523b62a6d946b9281762a2
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-10-08 10:42:32 +0600

    Updated

---
+++ models/Message.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const messageModel = mongoose.Schema(
+  {
+    message: {
+      type: String,
+      trim: true
+    },
+    chat:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref:'Chat'
+    },
+    sender:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref:'User'
+    },
+    // sender:{
+    //   type: String
+    // },
+  },{
+    timestamps: true
+  }
+);
+module.exports = mongoose.model('Message', messageModel);
