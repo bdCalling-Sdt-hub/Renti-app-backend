04965180accb8e9847364775fd2bde5cc771c8c5
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-10-11 21:16:11 +0600

    Updated

--- app.js
+++ app.js
@@ -120,8 +120,8 @@ app.use((error, req, res, next) => {
 
 // Start the server
 
-app.listen(3001, '192.168.10.14', () => {
-  console.log('Server started on port 3001');
+app.listen(3000, '192.168.10.14', () => {
+  console.log('Server started on port 3000');
 });
 
 module.exports = app;
