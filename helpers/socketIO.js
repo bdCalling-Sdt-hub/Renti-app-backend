9ef1feae1214b28e37a5de3eaa56f797457c403e
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-10-29 18:32:31 +0600

    updated

--- helpers/socketIO.js
+++ helpers/socketIO.js
@@ -5,33 +5,43 @@ const socketIO = (io) => {
   io.on('connection', (socket) => {
     console.log(`ID: ${socket.id} just connected`);
     socket.on('join-room', (data) => {
-      console.log('someone wants to join--->', data);
+      //console.log('someone wants to join--->', data);
       socket.join('room' + data.uid);
       socket.emit('join-check', 'You are in room: ' + data.uid);
     });
 
     socket.on('add-new-chat', async (data) => {
+      //console.log("data info---->", data.chatInfo)
       const chat = await addChat(data.chatInfo)
-      console.log(data.uid)
+      console.log("add-new chat--->", chat, data.uid)
       io.to('room' + data.uid).emit('new-chat', chat)
     })
     socket.on("join-chat", async (data) => {
+      socket.join('room' + data.uid)
+      console.log("join-chat info---->", data)
       const allChats = await getMessageByChatId(data.uid)
       io.to("room" + data.uid).emit('all-messages', allChats)
     })
-
     socket.on('add-new-message', async (data) => {
+      console.log("message info------->", data)
       const message = await addMessage(data)
-      console.log('--------> new message to be added', message)
+      console.log('new message---------> ', message)
       const allMessages = await getMessageByChatId(message.chat)
+      console.log('all messages list----> ', allMessages)
       io.to('room' + message.chat).emit('all-messages', allMessages)
     })
-    
     socket.on('get-all-chats', async (data) => {
       const allChats = await getChatByParticipantId(data.uid)
-      //console.log('hitting from socket -------->', allChats)
-      io.to('room' + data).emit('all-chats', allChats)
+      socket.join('room' + data.uid)
+      console.log('hitting from socket -------->', allChats)
+      io.to('room' + data.uid).emit('all-chats', allChats)
     })
+
+    socket.on('leave-room', (data) => {
+      if (data?.uid) {
+        socket.leave('room' + data.uid);
+      }
+    });
 
     socket.on('disconnect', () => {
       console.log(`ID: ${socket.id} disconnected`);
