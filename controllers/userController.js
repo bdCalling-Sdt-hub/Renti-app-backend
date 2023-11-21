cd33ff9e8d7ecc15a7b0326dfc93216f5392432f
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-11-21 18:10:43 +0600

    Signup Updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,7 +118,6 @@ const userSignUp = async (req, res, next) => {
 };
 
 
-
 const signUp = async (req, res, next) => {
     const bankInfo = req.body.bankInfo
     const address = req.body.address
@@ -169,59 +168,8 @@ const signUp = async (req, res, next) => {
             imageFileName = `${req.protocol}://${req.get('host')}/public/uploads/image/${req.files.image[0].filename}`;
         }
 
-        // Create the user in the database
-        const user = await User.create({
-            fullName,
-            email,
-            phoneNumber, // Include phoneNumber
-            gender,
-            address,
-            dateOfBirth,
-            password,
-            image: imageFileName,
-            KYC: kycFileNames,
-            RFC,
-            creditCardNumber, // Corrected variable name
-            ine,
-            oneTimeCode,
-            role,
-            bankInfo
-        });
 
-
-        // Clear any previous timer for the user (if exists)
-        if (userTimers.has(user._id)) {
-            clearTimeout(userTimers.get(user._id));
-        }
-
-        // Set a new timer for the user to reset oneTimeCode after 3 minutes
-        const userTimer = setTimeout(async () => {
-            try {
-                user.oneTimeCode = null;
-                await user.save();
-                console.log(`oneTimeCode for user ${user._id} reset to null after 3 minutes`);
-                // Remove the timer reference from the map
-                userTimers.delete(user._id);
-            } catch (error) {
-                console.error(`Error updating oneTimeCode for user ${user._id}:`, error);
-            }
-        }, 180000); // 3 minutes in milliseconds
-
-        // Store the timer reference in the map
-        userTimers.set(user._id, userTimer);
-
-        // Prepare email for activate user
-        const emailData = {
-            email,
-            subject: 'Account Activation Email',
-            html: `
-          <h1>Hello, ${user.fullName}</h1>
-          <p>Your One Time Code is <h3>${oneTimeCode}</h3> to reset your password</p>
-          <small>This Code is valid for 3 minutes</small>
-          `,
-        };
-
-
+        // -----------------------------------------------------------
         const dateComponents = dateOfBirth.split("/");
 
         const day = parseInt(dateComponents[0]);
@@ -326,10 +274,8 @@ const signUp = async (req, res, next) => {
             },
         });
 
-
         console.log('stripeConnectAccount', account);
-        user.stripeConnectAccountId = account.id;
-        await user.save();
+
 
         const accountLink = await stripe.accountLinks.create({
             account: account.id,
@@ -342,22 +288,334 @@ const signUp = async (req, res, next) => {
 
         console.log('accountLink', accountLink);
 
+        // -------------------------------------------------------------------
+
+        if (account.id) {
+            // Create the user in the database
+            const user = await User.create({
+                fullName,
+                email,
+                phoneNumber, // Include phoneNumber
+                gender,
+                address,
+                dateOfBirth,
+                password,
+                image: imageFileName,
+                KYC: kycFileNames,
+                RFC,
+                creditCardNumber, // Corrected variable name
+                ine,
+                oneTimeCode,
+                role,
+                bankInfo
+            });
 
 
-        try {
-            emailWithNodemailer(emailData);
-            console.log(emailData);
-            return res.status(201).json({ message: 'Thanks! Please check your E-mail to verify.' });
-        } catch (emailError) {
-            console.error('Failed to send verification email', emailError);
+            // Clear any previous timer for the user (if exists)
+            if (userTimers.has(user._id)) {
+                clearTimeout(userTimers.get(user._id));
+            }
+
+            // Set a new timer for the user to reset oneTimeCode after 3 minutes
+            const userTimer = setTimeout(async () => {
+                try {
+                    user.oneTimeCode = null;
+                    await user.save();
+                    console.log(`oneTimeCode for user ${user._id} reset to null after 3 minutes`);
+                    // Remove the timer reference from the map
+                    userTimers.delete(user._id);
+                } catch (error) {
+                    console.error(`Error updating oneTimeCode for user ${user._id}:`, error);
+                }
+            }, 180000); // 3 minutes in milliseconds
+
+            // Store the timer reference in the map
+            userTimers.set(user._id, userTimer);
+
+            // Prepare email for activate user
+            const emailData = {
+                email,
+                subject: 'Account Activation Email',
+                html: `
+          <h1>Hello, ${user.fullName}</h1>
+          <p>Your One Time Code is <h3>${oneTimeCode}</h3> to reset your password</p>
+          <small>This Code is valid for 3 minutes</small>
+          `,
+            };
+
+
+            user.stripeConnectAccountId = account.id;
+            await user.save();
+
+            try {
+                emailWithNodemailer(emailData);
+                console.log(emailData);
+                return res.status(201).json({ message: 'Thanks! Please check your E-mail to verify.' });
+            } catch (emailError) {
+                console.error('Failed to send verification email', emailError);
+            }
         }
+        else {
+            return res.status(500).json({ message: 'Sign Up Faild! Please Check your Bank Information', error });
+        }
+
+
+
     } catch (error) {
         next(error);
         // return res.status(500).json({ message: 'Error creating user', error });
     }
 };
 
+
+// const signUp = async (req, res, next) => {
+//     const bankInfo = req.body.bankInfo
+//     const address = req.body.address
+
+//     console.log((address, bankInfo))
+
+//     try {
+//         const {
+//             fullName,
+//             email,
+//             phoneNumber, // Add phoneNumber to the request body
+//             gender,
+//             dateOfBirth,
+//             password,
+//             KYC,
+//             RFC,
+//             creditCardNumber, // Correct the variable name to creditCardNumber
+//             ine,
+//             image,
+//             role,
+//         } = req.body;
+
+
+//         // Check if the user already exists
+//         const userExist = await User.findOne({ email });
+//         if (userExist) {
+//             return res.status(409).json({ message: 'User already exists! Please login' });
+//         }
+
+//         // Generate OTC (One-Time Code)
+//         const oneTimeCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+
+//         const kycFileNames = [];
+
+
+//         if (req.files && req.files.KYC) {
+//             req.files.KYC.forEach((file) => {
+//                 // Add public/uploads link to each KYC file
+//                 const kycLink = `${req.protocol}://${req.get('host')}/public/uploads/kyc/${file.filename}`;
+//                 kycFileNames.push(kycLink);
+//             });
+//         }
+
+
+//         // Check if req.files.image exists and is an array
+//         if (req.files && Array.isArray(req.files.image) && req.files.image.length > 0) {
+//             // Add public/uploads link to the image file
+//             imageFileName = `${req.protocol}://${req.get('host')}/public/uploads/image/${req.files.image[0].filename}`;
+//         }
+
+//         // ----------------------------------------------------------------
+
+
+//         const dateComponents = dateOfBirth.split("/");
+
+//         const day = parseInt(dateComponents[0]);
+//         const month = parseInt(dateComponents[1]);
+//         const year = parseInt(dateComponents[2]);
+
+//         const fileUpload = await stripe.files.create({
+//             purpose: 'identity_document',
+//             file: {
+//                 data: fs.readFileSync(req.files.KYC[0].path),
+//                 name: req.files.KYC[0].filename, // Replace with the actual file name
+//                 type: req.files.KYC[0].mimetype, // Replace with the actual file type
+//             },
+//         });
+
+//         console.log("fileUpload")
+//         console.log("fileUpload", fileUpload)
+
+//         const backFileUpload = await stripe.files.create({
+//             purpose: 'identity_document',
+//             file: {
+//                 data: fs.readFileSync(req.files.KYC[0].path),
+//                 name: req.files.KYC[0].filename, // Replace with the actual file name
+//                 type: req.files.KYC[0].mimetype, // Replace with the actual file type
+//             },
+//         });
+
+//         // Get the file IDs from the uploads
+//         const frontFileId = fileUpload.id;
+//         const backFileId = backFileUpload.id;
+
+//         const account = await stripe.accounts.create({
+//             country: address.country,
+//             type: 'custom',
+//             business_type: 'individual',
+//             email: email,
+//             tos_acceptance: {
+//                 // service_agreement: 'recipient',   // MX er somoy ata off thakbe
+//                 ip: req.ip,
+//                 date: Math.floor(new Date().getTime() / 1000)
+//             },
+//             business_profile: {
+//                 mcc: '7512',
+//                 name: fullName,
+//                 product_description: 'Your business description',
+//                 support_address: {
+//                     city: 'Mexico',
+//                     country: 'MX',
+//                     line1: 'Mexico',
+//                     line2: 'Mexico',
+//                     postal_code: '22056',
+//                     state: 'Aguascalientes',
+//                 },
+//             },
+//             company: {
+//                 address: {
+//                     city: 'Mexico',
+//                     country: 'MX',
+//                     line1: 'Mexico',
+//                     line2: 'Mexico',
+//                     postal_code: '22056',
+//                     state: 'Aguascalientes',
+//                 },
+//             },
+//             individual: {
+//                 dob: {
+//                     day: day,
+//                     month: month,
+//                     year: year,
+//                 },
+//                 email: email,
+//                 first_name: fullName,
+//                 last_name: ' ',
+//                 id_number: ine,
+//                 phone: phoneNumber,
+//                 address: {
+//                     city: address.city,
+//                     country: address.country,
+//                     line1: address.line1,
+//                     postal_code: address.postal_code,
+//                     state: address.state,
+//                 },
+//                 verification: {
+//                     document: {
+//                         front: frontFileId, // Replace with the actual file path
+//                         back: backFileId, // Replace with the actual file path
+//                     },
+//                 },
+//             },
+//             capabilities: {
+//                 transfers: {
+//                     requested: true,
+//                 },
+//             },
+//             external_account: {
+//                 object: 'bank_account',
+//                 country: 'MX',
+//                 currency: 'mxn',
+//                 account_holder_name: bankInfo.account_holder_name,
+//                 account_holder_type: bankInfo.account_holder_type,
+//                 account_number: bankInfo.account_number,
+//             },
+//         });
+
+
+//         console.log('stripeConnectAccount', account);
+
+
+//         const accountLink = await stripe.accountLinks.create({
+//             account: account.id,
+//             refresh_url: 'https://example.com/reauth',
+//             return_url: 'https://example.com/return',
+//             type: 'account_onboarding',
+//             collect: 'eventually_due'
+//         });
+
+
+//         console.log('accountLink', accountLink);
+
+//         // ----------------------------------------------------------------
+
+//         // Create the user in the database
+//         const user = await User.create({
+//             fullName,
+//             email,
+//             phoneNumber, // Include phoneNumber
+//             gender,
+//             address,
+//             dateOfBirth,
+//             password,
+//             image: imageFileName,
+//             KYC: kycFileNames,
+//             RFC,
+//             creditCardNumber, // Corrected variable name
+//             ine,
+//             oneTimeCode,
+//             role,
+//             bankInfo
+//         });
+
+//         user.stripeConnectAccountId = account.id;
+//         await user.save();
+
+
+//         // Clear any previous timer for the user (if exists)
+//         if (userTimers.has(user._id)) {
+//             clearTimeout(userTimers.get(user._id));
+//         }
+
+//         // Set a new timer for the user to reset oneTimeCode after 3 minutes
+//         const userTimer = setTimeout(async () => {
+//             try {
+//                 user.oneTimeCode = null;
+//                 await user.save();
+//                 console.log(`oneTimeCode for user ${user._id} reset to null after 3 minutes`);
+//                 // Remove the timer reference from the map
+//                 userTimers.delete(user._id);
+//             } catch (error) {
+//                 console.error(`Error updating oneTimeCode for user ${user._id}:`, error);
+//             }
+//         }, 180000); // 3 minutes in milliseconds
+
+//         // Store the timer reference in the map
+//         userTimers.set(user._id, userTimer);
+
+//         // Prepare email for activate user
+//         const emailData = {
+//             email,
+//             subject: 'Account Activation Email',
+//             html: `
+//           <h1>Hello, ${user.fullName}</h1>
+//           <p>Your One Time Code is <h3>${oneTimeCode}</h3> to reset your password</p>
+//           <small>This Code is valid for 3 minutes</small>
+//           `,
+//         };
+
+
+//         try {
+//             emailWithNodemailer(emailData);
+//             console.log(emailData);
+//             return res.status(201).json({ message: 'Thanks! Please check your E-mail to verify.' });
+//         } catch (emailError) {
+//             console.error('Failed to send verification email', emailError);
+//         }
+
+
+//     } catch (error) {
+//         next(error);
+//         // return res.status(500).json({ message: 'Error creating user', error });
+//     }
+// };
+
 // Verify email
+
+
 const verifyEmail = async (req, res, next) => {
     try {
         const { oneTimeCode, email } = req.body;
