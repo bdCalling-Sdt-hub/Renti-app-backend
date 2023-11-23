921d9cbe77f99eacfa9948ad4d71b749133337e1
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-11-23 15:59:24 +0600

    SMTP Work Updated

--- helpers/email.js
+++ helpers/email.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: "mail.privateemail.com",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
